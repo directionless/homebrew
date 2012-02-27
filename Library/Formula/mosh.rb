14b0868e85441af2430682a89c52b0b2dc8fd7a1
seph <seph@directionless.org>
2012-02-27 17:26:07 -0500

    simple recipe for mosh. I expect it will evolve.

--- Library/Formula/mosh.rb
+++ Library/Formula/mosh.rb
@@ -1,19 +1,25 @@
 require 'formula'
 
 class Mosh < Formula
-  url 'http://mosh-scheme.googlecode.com/files/mosh-0.2.7.tar.gz'
-  homepage 'http://mosh.monaos.org'
-  md5 '268598897536ff352296a905879940ad'
-
-  depends_on 'gmp'
-  depends_on 'oniguruma'
-
-  fails_with_llvm "Inline asm errors"
+  url 'https://github.com/quentinmit/mosh/tarball/ac7cd3aa2bcaf04b1b8fdbb2d41289039ca94550'
+  #homepage 'http://mosh.mit.edu/'
+  homepage 'https://github.com/keithw/mosh/'
+  md5 '328edd001ea64cb4c17eacdf91ab8087'
+  depends_on 'protobuf'
+  depends_on 'boost'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make"
+    # configure.ac erroniously specifies a too-new version of
+    # autoconf. Whomp it.
+    inreplace 'configure.ac', /AC_PREREQ.*/, 'AC_PREREQ([2.61])'
+
+    # There are some boost related warnings in here. They don't seem
+    # important, so ignore them.
+    inreplace 'src/frontend/Makefile.am', '-Werror', ''
+
+    # go go autotools
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
