bc66144a8bb36a9008e1ae0a4cbc84ae649942ec
Keith Winstein <keithw@mit.edu>
2012-03-11 04:44:25 -0300

    Compile with optimization and eliminate head (which won't work without autotools)

--- Library/Formula/mosh.rb
+++ Library/Formula/mosh.rb
@@ -1,17 +1,16 @@
 require 'formula'
 
 class Mosh < Formula
-  #homepage 'http://mosh.mit.edu/'
-  homepage 'https://github.com/keithw/mosh/'
+  homepage 'http://mosh.mit.edu/'
   url 'https://github.com/downloads/keithw/mosh/mosh-0.98c.tar.gz'
   md5 'ea7b11726d64b9125f40e5bf9cbd4921'
-  head 'https://github.com/keithw/mosh.git'
 
   depends_on 'pkg-config' => :build
   depends_on 'protobuf'
   depends_on 'boost'
 
   def install
+    ENV.O2
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
